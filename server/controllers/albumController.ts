e85780139b69c51ac1618d02c0cf7aa839f0fe89
RAVIROCK916 <prt15@iitbbs.ac.in>
2024-05-21 23:42:55 +0530

    feat(api): album genre routes and controller

--- server/controllers/albumController.ts
+++ server/controllers/albumController.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import type { Response } from "express";
-import { ALBUM_URL, NEW_ALBUM_RELEASES_URL } from "../constants/api-urls";
+import SPOTIFY_BASE_URL, {
+	ALBUM_URL,
+	NEW_ALBUM_RELEASES_URL,
+} from "../constants/api-urls";
 import type { AuthenticatedRequest } from "../types/requests";
-
 
 export const getAlbums = async (req: AuthenticatedRequest, res: Response) => {
 	const { token } = req;
@@ -34,10 +36,10 @@ export const getNewReleases = async (
 		.get(NEW_ALBUM_RELEASES_URL, {
 			headers: {
 				Authorization: `Bearer ${token}`,
-      },
-      params: {
-        limit: 5,
-      }
+			},
+			params: {
+				limit: 5,
+			},
 		})
 		.then((response) => {
 			res.json(response.data.albums.items);
@@ -48,6 +50,67 @@ export const getNewReleases = async (
 				.status(500)
 				.json({ error: "An error occurred while fetching the albums!!!" });
 		});
+};
+
+export const getGenres = async (req: AuthenticatedRequest, res: Response) => {
+	const { token } = req;
+
+	const genres = ["pop", "rock", "jazz", "classical", "hip-hop"];
+
+	try {
+		const genrePromises = genres.map(async (genre) => {
+			const response = await axios.get(`${SPOTIFY_BASE_URL}/recommendations`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+				params: {
+					seed_genres: genre,
+					limit: 10,
+				},
+			});
+
+			return {
+				genre,
+				tracks: response.data.tracks,
+			};
+		});
+
+		const recommendationsByGenre = await Promise.all(genrePromises);
+
+		res.json(recommendationsByGenre);
+	} catch (error: any) {
+		console.error(error.message);
+		res
+			.status(500)
+			.json({ error: "An error occurred while fetching the genres!!!" });
+	}
+	res.send("Genres sent");
+};
+
+export const getGenreAlbums = async (
+	req: AuthenticatedRequest,
+	res: Response
+) => {
+	const { token } = req;
+
+	try {
+		const response = await axios.get(`${SPOTIFY_BASE_URL}/recommendations`, {
+			params: {
+				seed_genres: req.params.name,
+				limit: 5,
+			},
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		res.json(response.data);
+	} catch (error: any) {
+		console.error(error.message);
+		res
+			.status(500)
+			.json({ error: "An error occurred while fetching the genres!!!" });
+	}
+	res.send("Genres sent");
 };
 
 export const getAlbum = async (req: AuthenticatedRequest, res: Response) => {
--- server/routes/albumRoute.ts
+++ server/routes/albumRoute.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
 	getAlbum,
 	getAlbums,
+	getGenreAlbums,
+	getGenres,
 	getNewReleases,
 } from "../controllers/albumController";
 
@@ -9,6 +11,8 @@ const router = Router();
 
 router.route("/").get(getAlbums);
 router.route("/new-releases").get(getNewReleases);
+router.route("/genres").get(getGenres);
+router.route("/genres/:name").get(getGenreAlbums);
 router.route("/:id").get(getAlbum);
 
 export default router;
